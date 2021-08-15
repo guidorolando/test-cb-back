d9757cdb6a6e64c7efe45e34f117c8704622987d
guido <guidorolando91@gmail.com>
2021-08-15 14:09:37 -0400

    initial commit

---
+++ src/main/java/com/test/cbback/CbBackApplication.java
@@ -0,0 +1,13 @@
+package com.test.cbback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/cbback/CbBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.cbback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
